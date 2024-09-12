aaffca56abc7d4942f7b0f5b76eb1fad7b2f7615
abgnv <bogunovaleksandar@gmail.com>
2024-09-12 13:39:23 +0300

    change repo name test commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { scrapeJobListings } from "./scrapers/devbgScraper.js";
+import { receiveMessages } from "./scrapers/consumer.js";
 
 async function main() {
+    // receiveMessages();
     const jobListings = await scrapeJobListings();
-    console.log(jobListings);
+    // console.log(jobListings);
 }
 
 main();
