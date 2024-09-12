79e836bc21df766d4663533eadaf001ff6c2bf7c
abgnv <bogunovaleksandar@gmail.com>
2024-09-12 13:59:20 +0300

    changed folder structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import { scrapeJobListings } from "./scrapers/devbgScraper.js";
 import { receiveMessages } from "./scrapers/consumer.js";
 
 async function main() {
-    // receiveMessages();
+    receiveMessages();
     const jobListings = await scrapeJobListings();
-    // console.log(jobListings);
 }
 
 main();
--- src/scrapers/devbgScraper.js
+++ src/scrapers/devbgScraper.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import mysql from 'mysql2';
+import aws from 'aws-sdk';
 
 
 // const connection = mysql.createConnection({
@@ -10,20 +11,24 @@ import mysql from 'mysql2';
 //     port: 33020
 // });
 
-const connection = mysql.createConnection({
-    host: 'database-1.ctmeg0eqc4g3.eu-north-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'sakatards',
-    database: 'innodb',
-    port: 3306
-});
+aws.config.update({ region: 'eu-north-1' });  // Set your region, e.g., 'us-east-1'
 
-connection.connect((err) => {
-    if (err) {
-        return console.error('error connecting: ' + err.stack);
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+const sqs = new aws.SQS();
+
+// const connection = mysql.createConnection({
+//     host: 'database-1.ctmeg0eqc4g3.eu-north-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'sakatards',
+//     database: 'innodb',
+//     port: 3306
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         return console.error('error connecting: ' + err.stack);
+//     }
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 async function scrapeJobListings() {
     const browser = await puppeteer.launch({ headless: false });
@@ -64,45 +69,56 @@ async function scrapeJobListings() {
                 title: document.querySelector('.job-title').innerText.trim(),
                 // time: document.querySelector('time').innerText,
                 // location: document.querySelector('.badge').innerText.trim(),
-                description: document.querySelector('.job_description')?.innerText.trim() ? document.querySelector('.job_description').innerText.trim() : 'hardcoded description',
-                icons: Array.from(document.querySelectorAll('.attachment-medium.size-medium'))
-                    .map(img => img.title.trim()),
-                categories: Array.from(document.querySelectorAll('.pill'))
-                    .map(a => a.innerText.trim()),
-                company: {
-                    // description: document.querySelector('.box-company p').innerText.trim(),
-                    profile: document.querySelector('.box-company a').href.trim(),
-                    logo: document.querySelector('.company-logo').getAttribute('src').trim()
-                },
+                // description: document.querySelector('.job_description')?.innerText.trim() ? document.querySelector('.job_description').innerText.trim() : 'hardcoded description',
+                // icons: Array.from(document.querySelectorAll('.attachment-medium.size-medium'))
+                //     .map(img => img.title.trim()),
+                // categories: Array.from(document.querySelectorAll('.pill'))
+                //     .map(a => a.innerText.trim()),
+                // company: {
+                //     // description: document.querySelector('.box-company p').innerText.trim(),
+                //     profile: document.querySelector('.box-company a').href.trim(),
+                //     logo: document.querySelector('.company-logo').getAttribute('src').trim()
+                // },
             };
             return jobData;
         });
         // time_posted, 
-        const query = 'INSERT INTO job_listings5 (job_title, job_description, company_profile_url, company_logo_url, icon_titles, category_names) VALUES (?, ?, ?, ?, ?, ?)';
+        // const query = 'INSERT INTO job_listings5 (job_title, job_description, company_profile_url, company_logo_url, icon_titles, category_names) VALUES (?, ?, ?, ?, ?, ?)';
 
-        connection.query(query, [
-            job.title,
-            // job.time,
-            // job.location,
-            job.description,
-            // job.company.description,
-            job.company.profile,
-            job.company.logo,
-            job.icons.join(','),
-            job.categories.join(',')],
-            (error, results) => {
-                if (error) {
-                    console.error('Error inserting data:', error);
-                    return;
-                }
-                console.log('Data successfully inserted:', results);
-            });
+        // connection.query(query, [
+        //     job.title,
+        //     // job.time,
+        //     // job.location,
+        //     job.description,
+        //     // job.company.description,
+        //     job.company.profile,
+        //     job.company.logo,
+        //     job.icons.join(','),
+        //     job.categories.join(',')],
+        //     (error, results) => {
+        //         if (error) {
+        //             console.error('Error inserting data:', error);
+        //             return;
+        //         }
+        //         console.log('Data successfully inserted:', results);
+        //     });
 
+        const params = {
+            MessageBody: JSON.stringify(job),
+            QueueUrl: 'https://sqs.eu-north-1.amazonaws.com/626635404006/Test2',
+        };
 
+        sqs.sendMessage(params, (err, data) => {
+            if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data.MessageId);
+            }
+        });
     }
 
     await browser.close();
-    connection.end();
+    // connection.end();
 
 }
 
